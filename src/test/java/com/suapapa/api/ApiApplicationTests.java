0aa75fd24d4bf6b315c6aaa755c242ed89209774
JoeSangHyun <tiamet133@naver.com>
2022-11-20 21:14:45 +0900

    커밋 1120

---
+++ src/test/java/com/suapapa/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suapapa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
